d8ffca8cf9eb07d1f9fe7bdd15f1875f3d8372a2
Uelisson <tagots09@gmail.com>
2025-10-24 16:46:09 -0300

    Adicionando efeito de aurora ao botão

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { Gradient } from '../../components/gradient/gradient.animation';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +14,26 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('gradientCanvas')
+  gradientCanvas!: ElementRef<HTMLCanvasElement>;
+  private gradient: Gradient;
+
+  constructor() {
+    this.gradient = new Gradient();
+  }
+
+  ngAfterViewInit(): void {
+    if (this.gradientCanvas && this.gradientCanvas.nativeElement) {
+      this.gradient.initGradient(this.gradientCanvas.nativeElement);
+    } else {
+      console.error('Elemento canvas do gradiente não foi encontrado!');
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.gradient) {
+      this.gradient.disconnect();
+    }
+  }
+}
