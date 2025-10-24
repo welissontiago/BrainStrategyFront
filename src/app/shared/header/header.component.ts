3ad23e15c5e898d91ad19dee71ea065e3c109ba6
Uelisson <tagots09@gmail.com>
2025-10-24 19:05:45 -0300

    Header completo com Tailwindcss

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,5 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnDestroy,
-  ViewChild,
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { Gradient } from '../../components/gradient/gradient.animation';
 
 @Component({
   selector: 'app-header',
@@ -14,26 +7,4 @@ import { Gradient } from '../../components/gradient/gradient.animation';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent implements AfterViewInit, OnDestroy {
-  @ViewChild('gradientCanvas')
-  gradientCanvas!: ElementRef<HTMLCanvasElement>;
-  private gradient: Gradient;
-
-  constructor() {
-    this.gradient = new Gradient();
-  }
-
-  ngAfterViewInit(): void {
-    if (this.gradientCanvas && this.gradientCanvas.nativeElement) {
-      this.gradient.initGradient(this.gradientCanvas.nativeElement);
-    } else {
-      console.error('Elemento canvas do gradiente não foi encontrado!');
-    }
-  }
-
-  ngOnDestroy(): void {
-    if (this.gradient) {
-      this.gradient.disconnect();
-    }
-  }
-}
+export class HeaderComponent {}
