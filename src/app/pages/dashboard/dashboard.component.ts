cf742f765e5e2a36c5c8c0ba44d0f5167f2352a1
Uelisson <tagots09@gmail.com>
2025-11-02 01:32:38 -0300

    Completa a logica de exibir os aprendizados

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -69,6 +69,7 @@ export class DashboardComponent implements OnInit {
       senha: ['', Validators.required],
       isAdmin: [false],
     });
+    this.loadAprendizados();
   }
 
   loadAprendizados() {
